5b026730300b4e8a36f53c52e93e9165c7921ddf
Adrian Todorov <adrian.todorov@outlook.com>
2020-03-31 13:33:47 -0400

    Changes

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -121,10 +121,10 @@ add_databases_to_bucardo() {
     db_id=$(db_attr $db_index id integer)
     db_pass=$(load_db_pass $db_index)
     #run_bucardo_command "del db db$db_id --force"
-    run_bucardo_command "add db db$db_id dbname=\"$(db_attr $db_index dbname string)\" \
-                                user=\"$(db_attr $db_index user string)\" \
-                                pass=\"$db_pass\" \
-                                host=\"$(db_attr $db_index host string)\""
+    run_bucardo_command "add db db$db_id dbname=\`$(db_attr $db_index dbname string)\` \
+                                user=\`$(db_attr $db_index user string)\` \
+                                pass=\`$db_pass\` \
+                                host=\`$(db_attr $db_index host string)\`"
     db_index=$(expr $db_index + 1)
   done
 }
